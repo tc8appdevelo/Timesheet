86262d122320c5eda94caaee114208d1b81c335e
tc8appdevelo <tc8appdevelo@gmail.com>
2022-04-27 13:21:01 -0500

    made Project controller and model

---
+++ db/migrate/20220427181917_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :description
+      t.float :completion
+      t.float :hours
+
+      t.timestamps
+    end
+  end
+end
