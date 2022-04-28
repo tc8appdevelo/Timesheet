a6dd06d7a21c32147e31259290d973f16eeac142
tc8appdevelo <tc8appdevelo@gmail.com>
2022-04-27 20:55:16 -0500

    added belongs_to and has_many associations for projects and employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,11 +1,14 @@
 class Employee < ApplicationRecord
-  # not sure if you put the calculations for what happenes
-  # when assigned a project in here and just use the method
-  # in the controller or what the convention is.
+
+  has_many :projects,
+    primary_key: :id,
+    foreign_key: :employee_id,
+    class_name: :Project
+
 
   def add_project(project)
     p project.total_hours
     p project.worked_hours
-    
   end
+  
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+  belongs_to :employee,
+    primary_key: :id,
+    foreign_key: :employee_id,
+    class_name: :Employee
+
+
 end
